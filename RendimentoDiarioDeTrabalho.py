196de761ed301620e5b42dcb96f35f02519a14b9
Gabriel Souza <95205382+GSnery@users.noreply.github.com>
2024-03-29 12:09:47 -0300

    Add files via upload

---
+++ RendimentoDiarioDeTrabalho.py
@@ -0,0 +1,12 @@
+#Variaveis
+PesoPeixe=float(input("Insira a quantidade de Kg "))
+KgEx=int(PesoPeixe-50)
+Multa=int(4*KgEx)
+
+#Condições
+if PesoPeixe<=50:
+    print("Abaixo do peso limite")
+
+else:
+    print(KgEx,"Kg a mais","Sua multa é de", Multa )
+
